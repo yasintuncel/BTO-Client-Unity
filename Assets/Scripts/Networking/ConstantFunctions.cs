9de4d9639eff46233aaff97fe46a812faeb3a7e8
Yasin TUNCEL <yasintuncel.eee@gmail.com>
2022-01-27 15:34:31 +0300

    creation of user login manager is done. close #26

---
+++ Assets/Scripts/Networking/ConstantFunctions.cs
@@ -0,0 +1,27 @@
+﻿// created by yasintuncel
+using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.Networking;
+
+namespace BattleTacticsOnline.Networking
+{
+    public static class ConstantFunctions
+    {
+        public static async Task<Sprite> GetRemoteTexture(string url)
+        {
+            UnityWebRequest req = UnityWebRequestTexture.GetTexture(url);
+
+            UnityWebRequestAsyncOperation op = req.SendWebRequest();
+
+            while (op.isDone == false) await Task.Delay(100);
+
+            if (req.result != UnityWebRequest.Result.Success)
+            {
+                return null;
+            }
+
+            Texture2D texture = DownloadHandlerTexture.GetContent(req);
+            return Sprite.Create(texture, new Rect(0, 0, texture.width, texture.height), new Vector2(.5f, .5f));
+        }
+    }
+}
---
+++ Assets/Scripts/Ui/UserLoginManager.cs
@@ -0,0 +1,49 @@
+﻿// created by yasintuncel
+using BattleTacticsOnline.Networking;
+using BattleTacticsOnline.Networking.Http;
+using BattleTacticsOnline.Networking.Http.Managers;
+using BattleTacticsOnline.Networking.Http.Models;
+using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace BattleTacticsOnline.Ui
+{
+    public class UserLoginManager : MonoBehaviour
+    {
+        public UserManager userManager;
+        [Space]
+        public Text txtName;
+        public Text txtLevel;
+        public Text txtGold;
+        [Space]
+        public Image imgProfile;
+
+        private void OnEnable()
+        {
+            userManager.dUserLogin += OnUserLogin;
+        }
+        private void OnDisable()
+        {
+            userManager.dUserLogin -= OnUserLogin;
+        }
+
+        void OnUserLogin(User user)
+        {
+            txtName.text = user.nickName;
+            txtLevel.text = "" + user.level.level;
+            txtGold.text = "" + user.gold.amount;
+            GetProfilePicture(HttpConstants.IDENTICONS + "/" + user.identicon + ".png");
+        }
+
+        async Task GetProfilePicture(string url)
+        {
+             Sprite sp = await ConstantFunctions.GetRemoteTexture(url);
+
+            if(sp != null)
+            {
+                imgProfile.sprite = sp;
+            }
+        }
+    }
+}
